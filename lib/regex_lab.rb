57f87333a690e094e24d15b107c83a4728f7e90f
Cat Gomez <catherinedgomez@gmail.com>
2020-07-27 03:16:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  (word[0][1] =~ /[un]/i) && (word.scan(/\w+ing/))
+  if (word[0][1] =~ /[un]/i) && (word.scan(/\w+ing/))
+    true
+  else
+    false
+  end
 end
 
 def words_five_letters_long(text)
