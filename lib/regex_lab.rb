b309eea6fded7f30e696ab037518022ec67a49a1
Cat Gomez <catherinedgomez@gmail.com>
2020-07-27 03:16:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if (word[0][1] =~ /[un]/i) && (word.scan(/\w+ing/))
+  if (text[0][1] =~ /[un]/i) && (text.scan(/\w+ing/))
     true
   else
     false
