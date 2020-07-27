6ce195659131f38b714d08609cfb4e4b06f07157
Cat Gomez <catherinedgomez@gmail.com>
2020-07-27 03:04:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word << %w(a e i o u).include?(word[0]) 
+  if word << %w(a e i o u).include?(word[0])
         # word.match.grep(/^\w[aeiou]\s/)
     true
   else
