e9a1bf89631ab986c313b38fc4198348d46a7335
Cat Gomez <catherinedgomez@gmail.com>
2020-07-27 03:17:41 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,11 +3,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if (text[0][1] =~ /[un]/i) && (text.scan(/\w+ing/))
-    true
-  else
-    false
-  end
+  (text[0][1] =~ /[un]/i) && (text.scan(/\w+ing/))
+
 end
 
 def words_five_letters_long(text)
